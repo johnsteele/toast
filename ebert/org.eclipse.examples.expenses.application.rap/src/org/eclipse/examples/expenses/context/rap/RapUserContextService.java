51eb19d0ee52bed0c3eba235f3158c2a1bdfac89
wbeaton <wbeaton>
2009-02-26 21:46:34 +0000

    Added some comments, pruned out some previously-commented out code.

--- ebert/org.eclipse.examples.expenses.application.rap/src/org/eclipse/examples/expenses/context/rap/RapUserContextService.java
+++ ebert/org.eclipse.examples.expenses.application.rap/src/org/eclipse/examples/expenses/context/rap/RapUserContextService.java
@@ -10,32 +10,35 @@
  *******************************************************************************/
 package org.eclipse.examples.expenses.context.rap;
 
+import org.eclipse.examples.expenses.application.rap.Activator;
 import org.eclipse.examples.expenses.context.IUserContext;
 import org.eclipse.examples.expenses.context.IUserContextService;
 import org.eclipse.rwt.RWT;
 import org.eclipse.rwt.service.ISessionStore;
+import org.osgi.framework.BundleContext;
 
+/**
+ * One instance of this class is created by the {@link Activator} on startup
+ * and registered as an Equinox/OSGi service (and is subsequently deregistered
+ * when the activator stops). This instance is responsible for providing
+ * user state in the application. 
+ * <p>
+ * This class is specific to RAP and leverages the RWT notion of state
+ * to manage an instance of {@link RapUserContext} for each user.
+ * 
+ * @see Activator#start(BundleContext)
+ * @see Activator#stop(BundleContext)
+ */
 public class RapUserContextService implements IUserContextService {
 	private static final String USER_CONTEXT_KEY = "user-context";
 
-	public RapUserContextService() {
-	}
-	
 	public IUserContext getUserContext() {
 		ISessionStore sessionStore = RWT.getSessionStore();
-		/*
-		 * Synchronize on the session store since there is some possibility
-		 * that multiple threads connected to the current user may try to do
-		 * this simultaneously (it's unlikely, but possible). This could happen,
-		 * for example, as a result of a call to Display#asyncExec(Runnable).
-		 */
-		//synchronized (sessionStore) {
-			RapUserContext context = (RapUserContext) sessionStore.getAttribute(USER_CONTEXT_KEY);
-			if (context == null) {
-				context = new RapUserContext();
-				sessionStore.setAttribute(USER_CONTEXT_KEY, context);
-			}
-			return context;
-		//}
+		RapUserContext context = (RapUserContext) sessionStore.getAttribute(USER_CONTEXT_KEY);
+		if (context == null) {
+			context = new RapUserContext();
+			sessionStore.setAttribute(USER_CONTEXT_KEY, context);
+		}
+		return context;
 	}
 }
--- ebert/org.eclipse.examples.expenses.views/src/org/eclipse/examples/expenses/context/IUserContextService.java
+++ ebert/org.eclipse.examples.expenses.views/src/org/eclipse/examples/expenses/context/IUserContextService.java
@@ -10,6 +10,31 @@
  *******************************************************************************/
 package org.eclipse.examples.expenses.context;
 
+/**
+ * Instances of this class are responsible for providing user state. This
+ * application can potentially service multiple user simultaneously (in the RAP
+ * case, for example), so storing state using traditional singletons simply
+ * doesn't work.
+ * <p>
+ * Implementors of this class are responsible for providing an instance
+ * of {@link IUserContext} for the user operating in the calling thread.
+ * <p>
+ * Implementors of this interface should register themselves as Equinox/OSGi
+ * services. In general, it makes sense for there to be exactly one registered
+ * service implementing this interface. However, clients should be prepared
+ * to handle the case where an instance is not registered (or hasn't been
+ * registered yet), or multiple instances are registered.
+ */
 public interface IUserContextService {
+	
+	/**
+	 * This method answers the instance of {@link IUserContext} for
+	 * the user operating in the current thread. To ensure that this
+	 * method answers the right context object, it should be called
+	 * from the UI thread.
+	 * 
+	 * @return an implementor of {@link IUserContext} containing
+	 * the state for the current user.
+	 */
 	IUserContext getUserContext();
 }
